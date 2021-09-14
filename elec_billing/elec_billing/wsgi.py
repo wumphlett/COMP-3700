e98c6e1eeb0e86494d8d98b613636a5a48fac285
Will Humphlett <will@humphlett.net>
2021-09-14 06:14:18 -0500

    Add code enforcement infrastructure

--- elec_billing/elec_billing/wsgi.py
+++ elec_billing/elec_billing/wsgi.py
@@ -2,14 +2,15 @@ import os
 import sys
 
 sys.path.append("/opt/bitnami/projects/elec_billing")
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/elec_billing/egg_cache")
+os.environ.setdefault(
+    "PYTHON_EGG_CACHE", "/opt/bitnami/projects/elec_billing/egg_cache"
+)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elec_billing.settings")
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application  # pylint: disable=wrong-import-position
 
 application = get_wsgi_application()
 
-from whitenoise import WhiteNoise
+from whitenoise import WhiteNoise  # pylint: disable=wrong-import-position
 
 application = WhiteNoise(application, root="/opt/bitnami/projects/elec_billing/static")
-
